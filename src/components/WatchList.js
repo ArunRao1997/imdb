d6ea1b593f560851890be46449685e84fac5aee4
ArunRao1997 <rao.nayineni1997@gmail.com>
2023-09-19 12:41:18 -0400

    Delete

--- src/components/WatchList.js
+++ src/components/WatchList.js
@@ -3,83 +3,10 @@ import React, { useState, useEffect } from 'react'
 const WatchList = () => {
 
     const [favorites, setFavorites] = useState([])
-    const [genres , setGenres] = useState([])
+    const [genres, setGenres] = useState([])
+    const [currGenre, setCurrGenre] = useState('All Genres')
+    const [rating, setRating] = useState(0)
 
-    let movies = [
-        {
-          adult: false,
-          backdrop_path: "/ogFIG0fNXEYRQKrpnoRJcXQNX9n.jpg",
-          id: 619930,
-          title: "Narvik",
-          original_language: "no",
-          original_title: "Kampen om Narvik",
-          overview:
-            "April, 1940. The eyes of the world are on Narvik, a small town in northern Norway, a source of the iron ore needed for Hitler's war machine. Through two months of fierce winter warfare, the German leader is dealt with his first defeat.",
-          poster_path: "/gU4mmINWUF294Wzi8mqRvi6peMe.jpg",
-          media_type: "movie",
-          genre_ids: [10752, 18, 36, 28],
-          popularity: 321.063,
-          release_date: "2022-12-25",
-          video: true,
-          vote_average: 7.406,
-          vote_count: 53,
-        },
-        {
-          adult: false,
-          backdrop_path: "/6RCf9jzKxyjblYV4CseayK6bcJo.jpg",
-          id: 804095,
-          title: "The Fabelmans",
-          original_language: "en",
-          original_title: "The Fabelmans",
-          overview:
-            "Growing up in post-World War II era Arizona, young Sammy Fabelman aspires to become a filmmaker as he reaches adolescence, but soon discovers a shattering family secret and explores how the power of films can help him see the truth.",
-          poster_path: "/d2IywyOPS78vEnJvwVqkVRTiNC1.jpg",
-          media_type: "movie",
-          genre_ids: [18],
-          popularity: 163.3,
-          release_date: "2022-11-11",
-          video: false,
-          vote_average: 8.02,
-          vote_count: 561,
-        },
-        {
-          adult: false,
-          backdrop_path: "/fTLMsF3IVLMcpNqIqJRweGvVwtX.jpg",
-          id: 1035806,
-          title: "Detective Knight: Independence",
-          original_language: "en",
-          original_title: "Detective Knight: Independence",
-          overview:
-            "Detective James Knight 's last-minute assignment to the Independence Day shift turns into a race to stop an unbalanced ambulance EMT from imperiling the city's festivities. The misguided vigilante, playing cop with a stolen gun and uniform, has a bank vault full of reasons to put on his own fireworks show... one that will strike dangerously close to Knight's home.",
-          poster_path: "/jrPKVQGjc3YZXm07OYMriIB47HM.jpg",
-          media_type: "movie",
-          genre_ids: [28, 53, 80],
-          popularity: 119.407,
-          release_date: "2023-01-20",
-          video: false,
-          vote_average: 6.6,
-          vote_count: 10,
-        },
-        {
-          adult: false,
-          backdrop_path: "/e782pDRAlu4BG0ahd777n8zfPzZ.jpg",
-          id: 555604,
-          title: "Guillermo del Toro's Pinocchio",
-          original_language: "en",
-          original_title: "Guillermo del Toro's Pinocchio",
-          overview:
-            "During the rise of fascism in Mussolini's Italy, a wooden boy brought magically to life struggles to live up to his father's expectations.",
-          poster_path: "/vx1u0uwxdlhV2MUzj4VlcMB0N6m.jpg",
-          media_type: "movie",
-          genre_ids: [16, 14, 18],
-          popularity: 754.642,
-          release_date: "2022-11-18",
-          video: false,
-          vote_average: 8.354,
-          vote_count: 1694,
-        },
-      ];
-      
 
     let genreids = {
         28: "Action",
@@ -110,20 +37,63 @@ const WatchList = () => {
         setFavorites(moviesFromLocalStorage)
     }, [])
 
-    useEffect(()=>{
-        let temp = movies.map((movie)=>genreids[movie.genre_ids[0]])
-        setGenres(["All Genres" , ...temp])
-      })
-    
-    
-    
+    useEffect(() => {
+        let temp = favorites.map((movie) => genreids[movie.genre_ids[0]])
+        temp = new Set(temp)
+        setGenres(["All Genres", ...temp])
+    })
+
+
+    let filteredArray = []
+
+    //Genre Filter
+    filteredArray =
+        currGenre == 'All Genres'
+            ? favorites
+            : favorites.filter((movie) =>
+                genreids[movie.genre_ids[0]] == currGenre)
+
+    // Sorting w.r.t Ratings
+    if (rating == -1) {
+        filteredArray = filteredArray.sort(function (objA, objB) {
+            return objB.vote_average - objA.vote_average
+        })
+    }
+
+    if (rating == 1) {
+        filteredArray = filteredArray.sort(function (objA, objB) {
+            return objA.vote_average - objB.vote_average
+        })
+    }
+
+    // Sorting w.r.t Popularity
+
+
+
+
+
+
+
+    const del = (movie) => {
+        let newArray = favorites.filter((m) => m.id != movie.id)
+        setFavorites([...newArray])
+        localStorage.setItem('imdb', JSON.stringify(newArray))
+    }
+
 
     return (
 
         <>
             <div className="mt-6 flex space-x-2 justify-center">
                 {genres.map((genre) => {
-                    return <div>{genre}</div>
+                    return <button className={currGenre == genre ? "m-2 text-lg p-1 px-2 bg-blue-400 text-white rounded-xl font-bold"
+                        : "m-2 text-lg p-1 px-2 bg-gray-400 hover:bg-blue-400 text-white rounded-xl font-bold"
+                    }
+                        onClick={() => {
+                            setCurrGenre(genre)
+                        }}
+                    >
+                        {genre}</button>
                 })}
             </div>
 
@@ -134,7 +104,21 @@ const WatchList = () => {
                             <th class='px-6 py-4 font-medium text-gray-900'>Name</th>
                             <th>
                                 <div className='flex'>
+                                    <img
+                                        src="https://img.icons8.com/external-those-icons-lineal-those-icons/24/000000/external-up-arrows-those-icons-lineal-those-icons-3.png"
+                                        className="mr-1"
+                                        onClick={() => {
+                                            setRating(1)
+                                        }}
+                                    />
                                     <div>Ratings</div>
+                                    <img
+                                        src="https://img.icons8.com/external-those-icons-lineal-those-icons/24/000000/external-down-arrows-those-icons-lineal-those-icons-4.png"
+                                        className="ml-1"
+                                        onClick={() => {
+                                            setRating(-1)
+                                        }}
+                                    />
                                 </div>
                             </th>
                             <th>
@@ -151,7 +135,7 @@ const WatchList = () => {
                     </thead>
 
                     <tbody class='divide-y divide-gray-100 border-t border-gray-100'>
-                        {favorites.map((movie) => {
+                        {filteredArray.map((movie) => {
                             return <tr class='hover:bg-gray-50'>
                                 <td class='flex items-center px-6 py-4 font-normal text-gray-900 space-x-2'>
 
@@ -168,8 +152,15 @@ const WatchList = () => {
                                     {movie.popularity}
                                 </td>
 
-                                <td className='pl-3  py-4'>
-                                    Action
+                                <td className='py-4'>
+                                    {genreids[movie.genre_ids[0]]}
+                                </td>
+
+                                <td>
+                                    <button className='text-red-600' onClick={() => del(movie)}>
+                                        Delete
+                                    </button>
+
                                 </td>
 
                             </tr>
